c2140cfef50bdf7cc6c6b69af9ea7880be4d7375
can özfuttu <84626499+cozfuttu@users.noreply.github.com>
2024-09-22 03:23:21 +0800

    feat: reown chains updated

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,25 +1,22 @@
+import { CaipNetwork } from '@reown/appkit'
 import { type Chain } from 'viem'
 
 export const zamaDevnet = {
-  id: 9000,
+  id: 'eip155:9000',
+  chainId: 9000,
   name: 'Zama Devnet',
-  nativeCurrency: { name: 'Zama', symbol: 'ZAMA', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://devnet.zama.ai'] },
-  },
-  blockExplorers: {
-    default: { name: 'Zamascan Devnet', url: 'https://explorer.devnet.zama.ai' },
-  },
-} as const satisfies Chain
+  currency: 'ZAMA',
+  rpcUrl: "https://devnet.zama.ai",
+  explorerUrl: "https://explorer.devnet.zama.ai",
+  chainNamespace: 'eip155',
+} as const satisfies CaipNetwork
 
 export const fhenixHelium = {
-  id: 8008135,
+  id: 'eip155:8008135',
+  chainId: 8008135,
   name: 'Fhenix Helium',
-  nativeCurrency: { name: 'tFHE', symbol: 'tFHE', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://api.helium.fhenix.zone'] },
-  },
-  blockExplorers: {
-    default: { name: 'Fhenix Helium Explorer', url: 'https://explorer.helium.fhenix.zone' },
-  },
-} as const satisfies Chain
+  currency: 'tFHE',
+  rpcUrl: "https://api.helium.fhenix.zone",
+  explorerUrl: "ttps://explorer.helium.fhenix.zone",
+  chainNamespace: 'eip155',
+} as const satisfies CaipNetwork

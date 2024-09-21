8215847d788b5e63d8c47e024fc3ebda3a48c9dc
Yagiz Avsar <63153577+AvsarYagiz@users.noreply.github.com>
2024-09-22 02:09:46 +0300

    feat: Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex w-full justify-center bg-black-dark p-2">
+      <p className="text-sm text-foreground/50">
+        © 2024 <strong>FHENTOM</strong>
+      </p>
+    </footer>
+  );
+}

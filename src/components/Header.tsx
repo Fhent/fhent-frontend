024867934e07d0c3ecab08bb65666d32cc8a217a
Yagiz Avsar <63153577+AvsarYagiz@users.noreply.github.com>
2024-09-21 07:41:10 +0300

    feat: Add logo to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
     <header className="flex w-full justify-center bg-black-dark shadow-black drop-shadow-lg">
       <div className="container flex items-center justify-between p-2">
         <div className="flex items-center gap-2">
-          {/* add logo instead of span later */}
-          <span className="size-10 bg-red-500" />
+          <Image
+            src="/logo/fhenton-logo-mark.png"
+            alt="fhenton-logo"
+            width={40}
+            height={40}
+          />
         </div>
         <div className="flex gap-8">
           <w3m-button />

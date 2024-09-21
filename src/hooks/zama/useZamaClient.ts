a5290e302af4b52b0da5212f45fdc01b2ba7db43
can özfuttu <84626499+cozfuttu@users.noreply.github.com>
2024-09-21 20:33:10 +0800

    feat: useZamaClient added

---
+++ src/hooks/zama/useZamaClient.ts
@@ -0,0 +1,37 @@
+import { zamaDevnet } from "@/config/chains";
+import { createInstance, FhevmInstance, initFhevm } from "fhevmjs";
+import { useEffect, useState } from "react";
+import { useAccount, useClient, useWalletClient } from "wagmi";
+
+const useZamaClient = () => {
+  const { address: account } = useAccount();
+  const [client, setClient] = useState<FhevmInstance | null>(null);
+  const { data: walletClient } = useWalletClient({ chainId: zamaDevnet.id })
+  const wagmiClient = useClient({ chainId: zamaDevnet.id });
+
+  useEffect(() => {
+    const init = async () => {
+      await initFhevm(); // Load needed WASM
+      const instance = await createInstance({
+        network: wagmiClient,
+        networkUrl: "https://devnet.zama.ai",
+        gatewayUrl: "https://gateway.devnet.zama.ai",
+
+      });
+
+      setClient(instance);
+    };
+
+    console.log("zama client: ", client);
+
+    if (account) init();
+  }, [
+    account,
+    // using walletclient instead of wagmiclient, as wagmiclient cannot be used in the hook
+    walletClient
+  ]);
+
+  return client;
+}
+
+export default useZamaClient;
